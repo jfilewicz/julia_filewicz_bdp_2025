dfe9ab28f8c3a3b6052504ee93751cc0336c5cc8
jfilewicz <147334516+jfilewicz@users.noreply.github.com>
2025-10-22 15:10:16 +0200

    Create zad5.sql

---
+++ 02_cwiczenia/zad5.sql
@@ -0,0 +1,18 @@
+INSERT INTO buildings (name, geom) VALUES
+('BuildingA', ST_GeomFromText('POLYGON((8 1.5, 10.5 1.5, 10.5 4, 8 4, 8 1.5))', 0)),
+('BuildingB', ST_GeomFromText('POLYGON((4 5, 6 5, 6 7, 4 7, 4 5))', 0)),
+('BuildingC', ST_GeomFromText('POLYGON((3 6, 5 6, 5 8, 3 8, 3 6))', 0)),
+('BuildingD', ST_GeomFromText('POLYGON((9 8, 10 8, 10 9, 9 9, 9 8))', 0)),
+('BuildingF', ST_GeomFromText('POLYGON((1 1, 2 1, 2 2, 1 2, 1 1))', 0));
+
+
+INSERT INTO roads (name, geom) VALUES
+('RoadX', ST_GeomFromText('LINESTRING(0 4.5, 12 4.5)', 0)),
+('RoadY', ST_GeomFromText('LINESTRING(7.5 0, 7.5 10.5)', 0));
+
+INSERT INTO poi (name, geom) VALUES
+('G', ST_GeomFromText('POINT(1 3.5)', 0)),
+('H', ST_GeomFromText('POINT(5.5 1.5)', 0)),
+('I', ST_GeomFromText('POINT(9.5 6)', 0)),
+('J', ST_GeomFromText('POINT(6.5 6)', 0)),
+('K', ST_GeomFromText('POINT(6 9.5)', 0));
