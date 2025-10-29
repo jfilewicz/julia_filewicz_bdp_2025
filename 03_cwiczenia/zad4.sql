ea7dfa299340fbe1b712b856b549ca61c8e6916e
jfilewicz <147334516+jfilewicz@users.noreply.github.com>
2025-10-29 12:51:44 +0100

    Create zad4.sql

---
+++ 03_cwiczenia/zad4.sql
@@ -0,0 +1,7 @@
+--4 W tabeli obiekty, jako obiekt7 zapisz obiekt złożony z obiektu 3 i obiektu 4.
+INSERT INTO obiekty (nazwa, geom)
+SELECT 
+    'obiekt7',
+    ST_Collect(a.geom, b.geom)
+FROM obiekty a, obiekty b
+WHERE a.nazwa = 'obiekt3' AND b.nazwa = 'obiekt4';
