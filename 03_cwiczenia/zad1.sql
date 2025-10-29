8afba25819ed08d4c68303955a37df2bd2f6b45e
jfilewicz <147334516+jfilewicz@users.noreply.github.com>
2025-10-29 12:38:06 +0100

    Create zad1.sql

---
+++ 03_cwiczenia/zad1.sql
@@ -0,0 +1,5 @@
+-- 1. Utwórz tabelę obiekty. W tabeli umieść nazwy i geometrie obiektów przedstawionych poniżej. Układ odniesienia ustal jako niezdefiniowany.
+CREATE TABLE obiekty ( 
+	nazwa TEXT,
+	geom GEOMETRY
+);
